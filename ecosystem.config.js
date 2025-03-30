bfc21b42f6334fa176bedb89018685e58ef8bdfe
Vierin <ivanvierin@gmail.com>
2025-03-30 18:53:00 +0200

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     apps: [
       {
-        name: "Convertor",
-        script: "npm",
-        args: "start",
-        cwd: "/home/ubuntu/Convertor"
-      }
-    ]
+        name: 'Convertor',
+        script: 'npm',
+        args: 'start',
+        cwd: '/home/ubuntu/Convertor',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
   };
